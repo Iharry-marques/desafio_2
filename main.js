84fb8c916c126f2978c42af3bb8e47b65bd1a246
Iharry-marques <fabio.marques.melo@gmail.com>
2024-10-17 18:29:50 -0300

    Desafio_2

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ Crie uma função que recebe como parâmetro a quantidade de vitórias e derrota
 depois disso retorne o resultado para uma variável, o saldo de Rankeadas deve ser feito através do calculo (vitórias - derrotas)
 
 Se vitórias for menor do que 10 = Ferro
-Se vitórias for entre 11 e 20 = Bronze
+Se vitórias for entre 11 e 20 = Bronzederrotas
 Se vitórias for entre 21 e 50 = Prata
 Se vitórias for entre 51 e 80 = Ouro
 Se vitórias for entre 81 e 90 = Diamante
@@ -28,3 +28,45 @@ Ao final deve se exibir uma mensagem:
 "O Herói tem de saldo de **{saldoVitorias}** está no nível de **{nivel}**"
  
 */
+
+
+function howManyWins(wins,losses){
+    let balance = wins - losses
+    let level
+    if (wins < 10) {
+        level = "Ferro";
+    } else if (wins >= 11 && wins <= 20) {
+        level = "Bronze";
+    } else if (wins >= 21 && wins <= 50) {
+        level = "Prata";
+    } else if (wins >= 51 && wins <= 80) {
+        level = "Ouro";
+    } else if (wins >= 81 && wins <= 90) {
+        level = "Diamante";
+    } else if (wins >= 91 && wins <= 100) {
+        level = "Lendário";
+    } else if (wins >= 101) {
+        level = "Imortal";
+    }
+    return {balance, level}
+}   
+
+
+let result = howManyWins(80,10)
+
+console.log(`O heroi tem o saldo de ${result.balance} vitorias e está no nivel ${result.level}`)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
